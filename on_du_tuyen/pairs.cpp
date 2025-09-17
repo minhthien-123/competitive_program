5ba58e7b9903e1c6cac9cb02c2e8490e1dbd32fb
Minh Thien <nmt04012009@gmail.com>
2025-09-17 16:21:16 +0700

    code lai may bai cu

--- on_du_tuyen/pairs.cpp
+++ on_du_tuyen/pairs.cpp
@@ -9,9 +9,9 @@
 #define ins insert
 
 const int maxn = 5e5;
-const int inf  = 1e18;
-const int mod  = 1e9 + 7;
-const int inv  = (mod + 1) / 2;
+const int inf = 1e18;
+const int mod = 1e9 + 7;
+const int inv = (mod + 1) / 2;
 
 int n, ans, a[maxn + 7];
 std::stack<int> st;
@@ -19,12 +19,13 @@ std::stack<int> st;
 signed main()
 {
     std::ios_base::sync_with_stdio(0);
-    std::cin.tie(0); std::cout.tie(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
 
-    if (std::fopen(task".inp", "r"))
+    if (std::fopen(task ".inp", "r"))
     {
-        std::freopen(task".inp", "r", stdin);
-        std::freopen(task".out", "w", stdout);
+        std::freopen(task ".inp", "r", stdin);
+        std::freopen(task ".out", "w", stdout);
     }
 
     std::cin >> n;
