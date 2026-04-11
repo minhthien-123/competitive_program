f993ec34237464783e8eeb74726cb9a928e5e663
minte <nmt04012009@gmail.com>
2026-04-11 20:03:50 +0700

    add lai cac code chua duoc add

--- bfs/labyrinth.cpp
+++ bfs/labyrinth.cpp
@@ -1,151 +1,229 @@
 #include <bits/stdc++.h>
 #define task "labyrinth"
-#define int long long
+#define __Thien_dep_trai__ signed main()
+#define ll long long
 #define ii std::pair<int, int>
+#define iii std::pair<ii, int>
+#define pll std::pair<ll, ll>
+#define vi std::vector<int>
+#define vii std::vector<ii>
 #define fi first
 #define se second
 #define pb push_back
+#define ins insert
+#define sz(x) ((int)(x).size())
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 
-const int maxn = 1e3 + 7;
-const int inf  = 1e18;
+const int maxn = 1e3;
+const int inf = 1e9;
+const int mod = 1e9 + 7;
+const int inv = (mod + 1) / 2;
+const int lg = 20;
 
-int m, n;
-ii res, resu;
-int dx[] = {-1,  0, 1, 0};
-int dy[] = { 0, -1, 0, 1};
-bool a[maxn][maxn], vis[maxn][maxn];
-int d[maxn][maxn];
-int ans = LLONG_MAX;
-char g[maxn][maxn];
-ii par[maxn][maxn];
-std::vector<ii> path;
-std::queue<ii> q;
-std::priority_queue<std::pair<int, ii>, std::vector<std::pair<int, ii>>, std::greater<std::pair<int, ii>>> pq;
-
-bool check(int x, int y){
-	if (x < 1 || y < 1 || x > m || y > n) return false;
-	return true;
+ll add(ll x, ll y)
+{
+	return (x + y + 2 * mod) % mod;
+}
+ll sub(ll x, ll y)
+{
+	return (x - y + 2 * mod) % mod;
+}
+ll mul(ll x, ll y)
+{
+	return (x * y) % mod;
+}
+ll power(ll x, ll y)
+{
+	if (y == 0)
+	{
+		return 1;
+	}
+	ll tmp = power(x, y / 2);
+	if (y % 2 == 0)
+	{
+		return (tmp * tmp) % mod;
+	}
+	else
+	{
+		return tmp * tmp % mod * x % mod;
+	}
 }
 
-void bfs(ii s){
-	while (pq.size()){
-		int dist = pq.top().fi;
-		ii t = pq.top().se;
-		pq.pop();
-		int x = t.fi, y = t.se;
+int n, m;
+int a[maxn + 7][maxn + 7];
+bool vis[maxn + 7][maxn + 7];
+int dist[maxn + 7][maxn + 7];
+ii p;
+ii trace[maxn + 7][maxn + 7];
+int res = inf;
+ii t = {0, 0};
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
-		if (a[x][y] != 0) continue;
+void bfs(ii s)
+{
+	if (a[s.fi][s.se] != 0)
+		return;
 
-		if (dist > d[x][y]) continue;
+	std::queue<ii> qu;
+	qu.push(s);
+	vis[s.fi][s.se] = true;
+	dist[s.fi][s.se] = 1;
+	trace[s.fi][s.se] = {-1, -1};
 
-		for (int i = 0; i < 4; i++){
-			int nx = x + dx[i], ny = y + dy[i];
-			if (check(nx, ny)){
-				if (a[nx][ny] != 1 && d[nx][ny] > d[x][y] + 1) {
-					d[nx][ny] = d[x][y] + 1;
-					par[nx][ny] = {x, y};
-					pq.push({d[nx][ny], {nx, ny}});
+	if (s.fi == 1 || s.fi == m || s.se == 1 || s.se == n)
+	{
+		res = 1;
+		t = s;
+		return;
+	}
+
+	while (sz(qu))
+	{
+		auto [x, y] = qu.front();
+		qu.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx >= 1 && nx <= m && ny >= 1 && ny <= n && !vis[nx][ny] && a[nx][ny] == 0)
+			{
+				vis[nx][ny] = true;
+				dist[nx][ny] = dist[x][y] + 1;
+				trace[nx][ny] = {x, y};
+				qu.push({nx, ny});
+
+				if (nx == 1 || nx == m || ny == 1 || ny == n)
+				{
+					res = dist[nx][ny];
+					t = {nx, ny};
+					return;
 				}
 			}
 		}
 	}
 }
 
-signed main(){
+__Thien_dep_trai__
+{
 	std::ios_base::sync_with_stdio(0);
-	std::cin.tie(0); std::cout.tie(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
 
-	std::freopen(task".inp", "r", stdin);
-	std::freopen(task".out", "w", stdout);
+	if (std::fopen(task ".inp", "r"))
+	{
+		std::freopen(task ".inp", "r", stdin);
+		std::freopen(task ".out", "w", stdout);
+	}
 
 	std::cin >> m >> n;
 
 	for (int i = 1; i <= m; i++)
-		for (int j = 1; j <= n; j++){
-			std::cin >> g[i][j];
-			par[i][j] = {-1, -1};
-		}
-
-	for (int i = 1; i <= m; i++)
+	{
 		for (int j = 1; j <= n; j++)
-			if (g[i][j] == '*'){
-				res = {i, j};
-				a[i][j] = 0;
-			}else if (g[i][j] == '.'){
-				a[i][j] = 0;
-			}else{
+		{
+			char c;
+			std::cin >> c;
+
+			if (c ==  '*')
+			{
+				p = {i, j};
+			}
+			else if (c == '#')
+			{
 				a[i][j] = 1;
 			}
-
-
-	for (int i = 1; i <= m; i++)
-		for (int j = 1; j <= n; j++)
-			d[i][j] = inf;
-	
-	pq.push({0, res});
-	d[res.fi][res.se] = 1;
-
-	/*std::cout << res.fi << " " << res.se << "\n";
-	for (int i = 1; i <= m; i++){
-		for (int j = 1; j <= n; j++)
-			std::cout << a[i][j] << " ";
-		std::cout << "\n";
-	}*/
-
-	bfs(res);
-
-	/*for (int i = 1; i <= m; i++){
-		for (int j = 1; j <= n; j++)
-			std::cout << d[i][j] * (d[i][j] != inf) << " ";
-		std::cout << "\n";
-	}*/
-
-	for (int i = 1; i <= m; i++){
-		for (int j = 1; j <= n; j++)
-			if ((i == 1 || j == 1 || i == m || j == n) && (d[i][j] != 0))
-				ans = std::min(ans, d[i][j]);
-	}
-
-	for (int i = 1; i <= m; i++)
-		for (int j = 1; j <= n; j++)
-			if ((d[i][j] == ans) && (i == 1 || j == 1 || i == m || j == n)){
-				resu = {i, j};
-				//std::cout << i << " " << j << "\n";
-				break;
+			else
+			{
+				a[i][j] = 0;
 			}
 
-	//std::cout << resu.fi << " " << resu.se << "\n";
-
-	//std::cout << par[res.fi][res.se].fi << " " << par[res.fi][res.se].se << "\n";
-
-	for (ii t = resu; t.fi != -1 ; t = par[t.fi][t.se])
-		path.pb(t);
-	std::reverse(path.begin(), path.end());
-
-	/*if (resu == res){
-		std::cout << 1 << "\n" << res.fi << " " << res.se << "\n";
-		return 0;
-	}
-
-	if (path.size() == 1){
-		std::cout << 0;
-		return 0;
-	}*/
-
-	for (int i = 1; i < (int)path.size(); i++){
-		ii x = path[i - 1], y = path[i];
-		if (x.fi == y.fi){
-			if (x.se < y.se)
-				std::cout << "E";
-			else
-				std::cout << "W";
-		}else if (x.se == y.se){
-			if (x.fi < y.fi)
-				std::cout << "S";
-			else
-				std::cout << "N";
+			dist[i][j] = inf;
 		}
 	}
+
+	bfs(p);
+
+	if (t.fi == 0 && t.se == 0)
+	{
+		std::cout << 0 << "\n";
+		return 0;
+	}
+
+	vii path;
+	int x = t.fi, y = t.se;
+
+	while (x != -1 && y != -1)
+	{
+		path.pb({x, y});
+		ii nxt = trace[x][y];
+		x = nxt.fi;
+		y = nxt.se;
+	}
+
+	std::reverse(path.begin(), path.end());
+
+	for (int i = 1; i < sz(path); i++)
+	{
+		int x = path[i].fi, y = path[i].se;
+		int lastx = path[i - 1].fi, lasty = path[i - 1].se;
+
+		if (x == lastx + 1)
+		{
+			std::cout << "S";
+		}
+		else if (x == lastx - 1)
+		{
+			std::cout << "N";
+		}
+		else if (y == lasty + 1)
+		{
+			std::cout << "E";
+		}
+		else
+		{
+			std::cout << "W";
+		}
+	}
+
+	std::cerr << "\nTime elapsed: " << TIME << " s.\n";
 
 	return 0;
 }
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣾⣿⣿⣿⣿⣷⣦⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⡿⡿⡿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⡿⠯⣩⣾⣾⣿⣿⣿⡚⣛⣝⢕⢕⢝⢝⢽⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⡿⣨⣿⣿⣿⣿⠿⣛⡭⢓⣿⣿⣷⣕⢷⣥⠥⢝⣿⣿⣿⡿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡿⢿⠟⣭⣿⣿⣿⣫⣾⡫⣯⢿⢷⢶⢿⣶⢮⣟⣩⣴⡻⢷⡆⣿⣿⢕⢙⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⡛⡡⠊⣴⣿⣿⣟⢽⢟⣿⣑⣙⣾⠺⣸⡾⡏⣚⡛⣿⣯⡡⢜⣧⢝⣿⣞⡪⡹⣿⣿⣿⡏⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡯⡫⢪⣒⠿⣽⣿⣟⣼⣿⣿⣕⣪⣿⠯⣫⣿⡯⡭⣮⣻⣿⣯⢝⣿⣿⣬⢼⣿⣪⡫⡻⣿⣿⣿⡍⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⣴⡿⡾⠺⡴⣾⣿⣿⢗⣟⣿⣿⠅⣶⣿⣯⢎⣾⡿⡪⣾⡧⣾⣿⠵⢕⣿⣿⣇⢵⣿⣏⡮⠪⣻⣿⡾⡮⡹⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣧⣿⣴⣿⣿⢓⣟⣿⢯⢏⣵⡿⣹⣿⣺⣿⡏⡇⣿⡿⣿⣿⢹⢘⣿⣿⣯⠕⣿⣿⣿⡬⣏⢻⣿⠯⣭⢺⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣟⣼⣿⣿⣿⣿⣿⢵⣿⣿⡯⡼⡲⣿⠗⣻⣿⣾⡟⢽⢺⣿⡧⣿⡿⡧⡣⣿⣿⡿⡟⣿⣿⣿⣷⢻⡼⣿⣿⣿⠲⣻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣀⠀⣀⣠⡤⣾⣞⣿⣿⣿⣿⣿⣟⡭⣿⣾⣿⠀⠨⣸⡏⠀⢹⣿⣿⠘⡗⢼⣿⣿⢿⢸⡺⠺⣿⣿⣟⡢⣿⣿⣿⣿⣿⣿⢜⣿⣿⡯⣪⣧⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠙⠚⠋⣡⢟⣾⣿⣿⣿⣟⢿⠭⢵⣻⣿⣿⠀⢨⣺⡇⠀⠸⣿⣿⠀⠹⢝⣿⣿⣿⡇⢯⡪⡮⣿⡯⡪⢿⣿⣿⣿⣿⣿⣷⢛⣿⣿⣾⣾⠀⠀⠀⢀⡴⠏⠻⠛⡧⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡼⣻⣿⣿⣿⣿⡟⣗⣳⢼⣏⡻⡩⠿⢤⠼⢾⣿⠀⠀⠹⢿⣇⠀⠳⣽⣿⣿⣷⡈⢮⡪⣻⣟⣒⣻⣿⣿⣿⣿⣿⣿⣕⣿⣿⣿⣿⡆⠀⠀⠘⢷⡆⣠⣙⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢯⣽⣿⣟⣥⢹⣵⣿⢮⣿⣷⣴⣿⣶⣾⣦⣤⣄⠀⠀⠀⠈⠉⠁⠀⠈⠙⢻⣛⡻⡎⢻⣽⣯⣆⣯⣿⣿⣿⣿⣿⣿⣧⡺⢟⣱⠞⣦⣀⡀⠀⠈⠙⠉⠉⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠻⡎⣶⣿⣿⣫⣾⣿⠟⠋⠁⠀⠉⠉⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣥⣩⠛⠲⣼⣗⢧⢖⡺⣿⣯⣺⣻⣿⣿⡄⢯⡈⠀⠀⣴⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣯⢫⣿⣿⣿⣫⣿⣿⡗⠀⢀⠂⡄⠀⠀⠀⠀⠀⠐⠳⠒⠀⠀⠀⠀⠉⠉⠙⠿⢯⣒⣼⣯⢝⣿⣮⡻⣿⣧⣪⡻⣿⡏⡸⠧⠼⣦⠉⢳⠋⢹⡆⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⣿⣿⣿⣿⣿⣿⡧⠢⠃⡐⠀⠀⠀⡖⠒⠤⠤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⡀⡹⢿⣿⠗⣿⣿⣿⣾⣯⡿⡪⡿⣿⣞⡟⣵⠛⠁⠀⠀⠛⣶⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣽⣧⠆⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠈⠉⠙⢳⡄⠀⡰⡜⡠⢁⠽⢿⣽⣿⣿⣿⣿⣿⣮⢮⣻⣗⣿⣬⣛⣿⣀⡽⠛⠋⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣷⣄⠀⠀⠀⠀⠹⡀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠇⠀⢠⢠⢁⠎⣠⣿⣿⣿⣿⣿⣿⢿⣿⣟⣽⣿⣿⣿⣿⠈⠉⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣰⣻⣿⣿⣿⢻⣿⣿⣿⣕⣿⣿⣿⣿⣷⣦⡀⠀⠀⠁⠀⠀⠀⠀⠀⣠⡤⠖⠁⠀⠀⠀⠁⠘⢎⣸⣺⣿⣿⣿⢿⣿⣽⣿⣿⣿⣿⣿⣿⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢰⣿⡿⣿⣿⢣⣿⢿⣿⢗⣽⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣀⣀⣀⣀⣀⣀⣀⣀⣠⣤⣴⣶⣿⣿⣿⡾⣿⣿⣷⢽⣷⣿⣿⣿⣿⣿⣿⣿⣾⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣾⣰⣿⣯⢞⣷⣿⣟⣅⣿⣿⣿⠿⠿⠿⠿⠿⠛⠛⠛⠿⣿⣯⡿⡫⡡⠽⣿⠽⠟⣿⣿⣿⣿⣿⣿⣺⣿⣿⣗⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣻⣆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣇⣿⣽⢳⣿⣿⠱⣼⡟⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣯⠾⠚⠉⠀⠀⠀⣟⢛⡿⠛⠉⣻⣿⣿⣿⣻⡧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣧⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢿⡿⣯⣽⣺⣿⣟⢴⢟⣥⣛⡇⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠋⠉⠀⠀⠀⠀⠀⠀⣻⠞⠁⠀⠀⣯⣿⣿⣿⣟⣕⣿⡿⣿⣿⣿⣿⣿⣿⣫⡯⣯⣿⣷⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⡺⣺⡿⠿⣾⣿⢣⡭⢿⠀⠹⠖⢶⡀⠀⠀⠀⠀⠀⡼⢭⣉⡀⠀⠀⠀⠀⠀⢀⡜⠁⠀⠀⠀⠸⣿⣿⣿⣿⢷⣗⢿⣿⡿⢋⣹⣿⣿⣿⣿⡪⡿⣾⣺⣿⣄⠀⠀⠀
+⠀⠀⠀⢀⡼⣯⣾⣯⣯⣽⣿⣿⠜⢦⡤⠀⠀⢲⡟⠀⠀⠀⠀⠀⢐⡇⠀⠀⠉⠛⠒⠶⠶⣶⠋⠀⠀⠀⠀⠀⣮⣾⣿⣿⣿⣟⣿⣝⢽⠞⣑⢑⣿⣿⣿⣿⡯⣯⣻⣯⣯⣽⣦⠀⠀
+⠀⠀⢠⣻⣮⣿⣟⣜⢿⢽⣿⣟⠀⡸⣤⡼⠷⡶⠇⠀⠀⠀⠀⠀⢸⢤⡀⠀⠀⠀⠀⢀⡴⠁⠀⠀⠀⠀⠀⢰⣻⣿⣿⢿⣿⡯⣿⣷⢿⣇⠆⠄⣿⣿⣿⣿⣿⡿⣾⣿⣷⡿⢿⣆⠀
+⠀⣰⣿⣷⣿⣿⣷⣝⣵⢽⡿⠫⠰⠸⢸⢸⠸⣿⢇⣀⣠⣄⠀⠀⢸⠀⠀⠀⠀⠉⢉⡟⠀⠀⠀⠀⠀⠀⠀⣇⣿⣿⡏⣾⣿⡿⣿⣿⣺⡻⣴⠙⢾⣿⣿⣿⣿⣷⣿⢿⣿⣿⣺⡻⡇
+⢸⡧⢽⣿⣿⣿⣿⠝⣷⠻⠟⢻⡆⠊⡮⠊⣇⠟⣿⡈⠃⠼⣆⠀⠸⠀⠀⠀⠀⢀⠎⠀⠀⠀⠀⠀⠀⠀⢰⣕⢿⣿⢿⢯⣿⣿⣿⣿⣮⡯⡷⣎⣻⣿⣿⣿⣿⣿⣿⣽⣿⣿⣫⡯⣧
+⠈⢿⢿⣿⣿⣿⣿⠇⡟⢀⠀⢈⣹⡆⣸⡯⢻⣌⠷⢶⣀⡶⠛⠀⠀⠀⠀⠀⢀⡎⠀⠀⠀⠀⠀⢀⣠⣲⡿⣗⣿⣿⣽⣗⣿⣿⣿⣿⣿⣿⡫⣿⣿⣿⣿⣿⣿⣿⣗⣿⣿⣿⡷⣪⣿
+⠀⠀⠛⣿⣿⣿⣿⢵⣽⣟⠦⠞⠁⡊⡪⣿⢨⡿⣯⣳⣭⣤⡀⠀⢠⠀⠀⠀⡘⠀⠀⠀⢀⣤⣾⡿⢋⣥⢾⢷⣷⢿⣇⢿⣏⣿⣿⣿⣿⣯⡿⣯⣺⣿⣿⣿⣵⣿⣿⣿⣿⣿⡯⡫⡎
+⠀⠀⠀⠈⢿⣿⣿⡟⣿⣧⣿⣬⠠⢀⠡⢣⣼⡧⠊⠻⣝⣿⣿⢆⢸⠀⠀⢰⠁⢀⣠⣾⡿⠟⣡⠔⠋⠔⡀⡼⣟⣾⣽⣄⣿⣝⣿⣿⣿⣷⣿⣿⢻⣎⣿⣽⣿⣿⡿⣿⣿⣿⡯⣻⠇
+*/
