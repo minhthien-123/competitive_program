84da1290836262953803f0f950aec780796948fd
minte <nmt04012009@gmail.com>
2026-09-17 10:35:54 +0700

    add

--- lop12/dung/something/rseq.cpp
+++ lop12/dung/something/rseq.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define task "rseq"
+#define int long long
 #define __Thien_dep_trai__ signed main()
 #define ll long long
 #define ii std::pair<int, int>
@@ -15,14 +16,16 @@
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
 #define bit_set(x, pos) ((x) |= (1ULL << (pos)))
 #define bit_clear(x, pos) ((x) &= ~(1ULL << (pos)))
+#define all(x) x.begin(), x.end()
 
 const int maxn = 1e5;
-const ll inf = 1e18;
+const ll INF = 1e18;
+const int inf = 1e9;
 const int mod = 1e9 + 7;
 const int inv = (mod + 1) / 2;
 const int lg = 20;
-const int dx[] = {-1, 1, 0, 0, -1, -1, 1, 1};
-const int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+const int dx[] = {0, 1, 0, -1, -1, -1, 1, 1};
+const int dy[] = {1, 0, -1, 0, -1, 1, -1, 1};
 
 ll add(ll x, ll y)
 {
@@ -34,7 +37,7 @@ ll sub(ll x, ll y)
 }
 ll mul(ll x, ll y)
 {
-    return (x * y) % mod;
+    return (x % mod * (y % mod)) % mod;
 }
 ll power(ll x, ll y)
 {
@@ -53,28 +56,23 @@ ll power(ll x, ll y)
     }
 }
 
-std::vector<int> adj[maxn + 7];
-int n, q;
-ll a[maxn + 7];
-int query[maxn + 7];
-bool flag[maxn + 7];
+int n, m;
+int a[maxn + 7], f[maxn + 7];
+bool flag[maxn + 7], check[maxn + 7];
 
 struct DSU
 {
-    std::vector<int> par, sz;
-    std::vector<ll> sum;
+    std::vector<int> par, sz, sum;
 
     DSU(int n)
     {
         par.resize(n + 7);
-        sz.resize(n + 7);
         sum.resize(n + 7);
-
+        sz.resize(n + 7);
         for (int i = 0; i <= n + 1; i++)
         {
             par[i] = i;
             sz[i] = 1;
-            sum[i] = a[i];
         }
     }
 
@@ -83,24 +81,82 @@ struct DSU
         return v == par[v] ? v : par[v] = acs(par[v]);
     }
 
-    void join(int a, int b)
+    void join(int u, int v)
     {
-        a = acs(a);
-        b = acs(b);
-        if (a != b)
+        u = acs(u);
+        v = acs(v);
+        if (u != v)
         {
-            if (sz[a] < sz[b])
+            if (sz[u] < sz[v])
             {
-                std::swap(a, b);
+                std::swap(u, v);
             }
-            par[b] = a;
-            sz[a] += sz[b];
-            sum[a] += sum[b];
+            par[v] = u;
+            sz[u] += sz[v];
+            sum[u] += sum[v];
         }
     }
 };
 
-ll res[maxn + 7];
+int res[maxn + 7];
+
+void solve()
+{
+    std::cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        std::cin >> a[i];
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        std::cin >> f[i];
+        flag[f[i]] = 1;
+    }
+
+    DSU dsu(n);
+
+    int cur = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (!flag[i])
+        {
+            check[i] = true;
+
+            dsu.sum[i] = a[i];
+            cur = std::max(cur, dsu.sum[i]);
+            if (i > 1 && check[i - 1])
+            {
+                dsu.join(i, i - 1);
+                cur = std::max(cur, dsu.sum[dsu.acs(i)]);
+            }
+        }
+    }
+    res[m] = cur;
+
+    for (int i = m; i >= 1; i--)
+    {
+        int pos = f[i];
+        check[pos] = 1;
+        dsu.sum[pos] = a[pos];
+        cur = std::max(cur, dsu.sum[pos]);
+        if (pos > 1 && check[pos - 1])
+        {
+            dsu.join(pos, pos - 1);
+        }
+        if (pos < n && check[pos + 1])
+        {
+            dsu.join(pos, pos + 1);
+        }
+        cur = std::max(cur, dsu.sum[dsu.acs(pos)]);
+        res[i - 1] = cur;
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        std::cout << res[i] << "\n";
+    }
+}
 
 __Thien_dep_trai__
 {
@@ -114,73 +170,11 @@ __Thien_dep_trai__
         std::freopen(task ".out", "w", stdout);
     }
 
-    std::cin >> n >> q;
-    for (int i = 1; i <= n; i++)
+    int tt = 1;
+    // std::cin >> tt;
+    while (tt--)
     {
-        std::cin >> a[i];
-    }
-    DSU dsu(n);
-
-    for (int i = 1; i <= q; i++)
-    {
-        std::cin >> query[i];
-        flag[query[i]] = true;
-        dsu.sum[query[i]] = 0;
-        dsu.sz[query[i]] = 0;
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (!flag[i])
-        {
-            if (!flag[i - 1])
-            {
-                dsu.join(i, i - 1);
-            }
-            if (!flag[i + 1])
-            {
-                dsu.join(i, i + 1);
-            }
-        }
-    }
-
-    ll ans = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (!flag[i])
-        {
-            ans = std::max(ans, dsu.sum[dsu.acs(i)]);
-        }
-        //std::cout << dsu.sum[dsu.acs(i)] << " ";
-    }
-
-    for (int i = q; i >= 1; i--)
-    {
-        res[i] = ans;
-
-        int u = query[i];
-
-        dsu.sum[u] = a[u];
-        dsu.sz[u] = 1;
-        flag[u] = false;
-        ans = std::max(ans, a[u]);
-
-        if (u > 1 && !flag[u - 1])
-        {
-            dsu.join(u, u - 1);
-            ans = std::max(ans, dsu.sum[dsu.acs(u)]);
-        }
-
-        if (u < n && !flag[u + 1])
-        {
-            dsu.join(u, u + 1);
-            ans = std::max(ans, dsu.sum[dsu.acs(u)]);
-        }
-    }
-
-    for (int i = 1; i <= q; i++)
-    {
-        std::cout << res[i] << "\n";
+        solve();
     }
 
     std::cerr << "\nTime elapsed: " << TIME << " s.\n";
---
+++ lop12/thien/segtree_advance/pst/P.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+#define task "P"
+#define int long long
+#define __Thien_dep_trai__ signed main()
+#define ll long long
+#define ii std::pair<int, int>
+#define iii std::pair<ii, int>
+#define pll std::pair<ll, ll>
+#define vi std::vector<int>
+#define vii std::vector<ii>
+#define fi first
+#define se second
+#define pb push_back
+#define ins insert
+#define sz(x) ((int)(x).size())
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+#define bit_set(x, pos) ((x) |= (1ULL << (pos)))
+#define bit_clear(x, pos) ((x) &= ~(1ULL << (pos)))
+#define all(x) x.begin(), x.end()
+
+const int maxn = 2e5;
+const ll INF = 1e18;
+const int inf = 1e9;
+const int mod = 1e9 + 7;
+const int inv = (mod + 1) / 2;
+const int lg = 20;
+const int dx[] = {0, 1, 0, -1, -1, -1, 1, 1};
+const int dy[] = {1, 0, -1, 0, -1, 1, -1, 1};
+
+ll add(ll x, ll y)
+{
+    return (x + y + 2 * mod) % mod;
+}
+ll sub(ll x, ll y)
+{
+    return (x - y + 2 * mod) % mod;
+}
+ll mul(ll x, ll y)
+{
+    return (x % mod * (y % mod)) % mod;
+}
+ll power(ll x, ll y)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+    ll tmp = power(x, y / 2);
+    if (y % 2 == 0)
+    {
+        return (tmp * tmp) % mod;
+    }
+    else
+    {
+        return tmp * tmp % mod * x % mod;
+    }
+}
+
+int n, q;
+int a[maxn + 7];
+
+const int max_nodes = 6000005;
+struct TrieXor
+{
+    int child[max_nodes][2];
+    int cnt[max_nodes];
+    int node_cnt;
+
+    void init()
+    {
+        node_cnt = 0;
+        memset(child[0], -1, sizeof(child[0]));
+        cnt[0] = 0;
+    }
+
+    int insert(int old, int x)
+    {
+        node_cnt++;
+        int curr = node_cnt;
+
+        for (int i = 29; i >= 0; i--)
+        {
+            int bit = (x >> i) & 1;
+
+            child[curr][0] = child[old][0];
+            child[curr][1] = child[old][1];
+
+            child[curr][bit] = ++node_cnt;
+            curr = child[curr][bit];
+            old = child[old][bit];
+            cnt[curr]++;
+        }
+        return curr;
+    }
+
+    int query_min(int root, int x)
+    {
+        int curr = root;
+        int ans = 0;
+        for (int i = 29; i >= 0; i--)
+        {
+            int bit = (x >> i) & 1;
+            if (child[curr][bit])
+            {
+                curr = child[curr][bit];
+            }
+            else
+            {
+                ans |= (1LL << i);
+                curr = child[curr][1 - bit];
+            }
+        }
+        return ans;
+    }
+
+    int query_max(int root, int x)
+    {
+        int curr = root;
+        int ans = 0;
+        for (int i = 29; i >= 0; i--)
+        {
+            int bit = (x >> i) & 1;
+            int target = 1 - bit;
+            if (child[curr][target])
+            {
+                ans |= (1LL << i);
+                curr = child[curr][target];
+            }
+            else
+            {
+                curr = child[curr][bit];
+            }
+        }
+        return ans;
+    }
+};
+
+void solve()
+{
+}
+
+__Thien_dep_trai__
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    if (std::fopen(task ".inp", "r"))
+    {
+        std::freopen(task ".inp", "r", stdin);
+        std::freopen(task ".out", "w", stdout);
+    }
+
+    int tt = 1;
+    // std::cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+
+    std::cerr << "\nTime elapsed: " << TIME << " s.\n";
+
+    return 0;
+}
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣾⣿⣿⣿⣿⣷⣦⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⡿⡿⡿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⡿⠯⣩⣾⣾⣿⣿⣿⡚⣛⣝⢕⢕⢝⢝⢽⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⡿⣨⣿⣿⣿⣿⠿⣛⡭⢓⣿⣿⣷⣕⢷⣥⠥⢝⣿⣿⣿⡿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⡿⢿⠟⣭⣿⣿⣿⣫⣾⡫⣯⢿⢷⢶⢿⣶⢮⣟⣩⣴⡻⢷⡆⣿⣿⢕⢙⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⡛⡡⠊⣴⣿⣿⣟⢽⢟⣿⣑⣙⣾⠺⣸⡾⡏⣚⡛⣿⣯⡡⢜⣧⢝⣿⣞⡪⡹⣿⣿⣿⡏⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡯⡫⢪⣒⠿⣽⣿⣟⣼⣿⣿⣕⣪⣿⠯⣫⣿⡯⡭⣮⣻⣿⣯⢝⣿⣿⣬⢼⣿⣪⡫⡻⣿⣿⣿⡍⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⣴⡿⡾⠺⡴⣾⣿⣿⢗⣟⣿⣿⠅⣶⣿⣯⢎⣾⡿⡪⣾⡧⣾⣿⠵⢕⣿⣿⣇⢵⣿⣏⡮⠪⣻⣿⡾⡮⡹⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣧⣿⣴⣿⣿⢓⣟⣿⢯⢏⣵⡿⣹⣿⣺⣿⡏⡇⣿⡿⣿⣿⢹⢘⣿⣿⣯⠕⣿⣿⣿⡬⣏⢻⣿⠯⣭⢺⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣟⣼⣿⣿⣿⣿⣿⢵⣿⣿⡯⡼⡲⣿⠗⣻⣿⣾⡟⢽⢺⣿⡧⣿⡿⡧⡣⣿⣿⡿⡟⣿⣿⣿⣷⢻⡼⣿⣿⣿⠲⣻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣀⠀⣀⣠⡤⣾⣞⣿⣿⣿⣿⣿⣟⡭⣿⣾⣿⠀⠨⣸⡏⠀⢹⣿⣿⠘⡗⢼⣿⣿⢿⢸⡺⠺⣿⣿⣟⡢⣿⣿⣿⣿⣿⣿⢜⣿⣿⡯⣪⣧⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠙⠚⠋⣡⢟⣾⣿⣿⣿⣟⢿⠭⢵⣻⣿⣿⠀⢨⣺⡇⠀⠸⣿⣿⠀⠹⢝⣿⣿⣿⡇⢯⡪⡮⣿⡯⡪⢿⣿⣿⣿⣿⣿⣷⢛⣿⣿⣾⣾⠀⠀⠀⢀⡴⠏⠻⠛⡧⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡼⣻⣿⣿⣿⣿⡟⣗⣳⢼⣏⡻⡩⠿⢤⠼⢾⣿⠀⠀⠹⢿⣇⠀⠳⣽⣿⣿⣷⡈⢮⡪⣻⣟⣒⣻⣿⣿⣿⣿⣿⣿⣕⣿⣿⣿⣿⡆⠀⠀⠘⢷⡆⣠⣙⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢯⣽⣿⣟⣥⢹⣵⣿⢮⣿⣷⣴⣿⣶⣾⣦⣤⣄⠀⠀⠀⠈⠉⠁⠀⠈⠙⢻⣛⡻⡎⢻⣽⣯⣆⣯⣿⣿⣿⣿⣿⣿⣧⡺⢟⣱⠞⣦⣀⡀⠀⠈⠙⠉⠉⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠻⡎⣶⣿⣿⣫⣾⣿⠟⠋⠁⠀⠉⠉⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣥⣩⠛⠲⣼⣗⢧⢖⡺⣿⣯⣺⣻⣿⣿⡄⢯⡈⠀⠀⣴⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣯⢫⣿⣿⣿⣫⣿⣿⡗⠀⢀⠂⡄⠀⠀⠀⠀⠀⠐⠳⠒⠀⠀⠀⠀⠉⠉⠙⠿⢯⣒⣼⣯⢝⣿⣮⡻⣿⣧⣪⡻⣿⡏⡸⠧⠼⣦⠉⢳⠋⢹⡆⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⣿⣿⣿⣿⣿⣿⡧⠢⠃⡐⠀⠀⠀⡖⠒⠤⠤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⡀⡹⢿⣿⠗⣿⣿⣿⣾⣯⡿⡪⡿⣿⣞⡟⣵⠛⠁⠀⠀⠛⣶⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣽⣧⠆⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠈⠉⠙⢳⡄⠀⡰⡜⡠⢁⠽⢿⣽⣿⣿⣿⣿⣿⣮⢮⣻⣗⣿⣬⣛⣿⣀⡽⠛⠋⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣷⣄⠀⠀⠀⠀⠹⡀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠇⠀⢠⢠⢁⠎⣠⣿⣿⣿⣿⣿⣿⢿⣿⣟⣽⣿⣿⣿⣿⠈⠉⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣰⣻⣿⣿⣿⢻⣿⣿⣿⣕⣿⣿⣿⣿⣷⣦⡀⠀⠀⠁⠀⠀⠀⠀⠀⣠⡤⠖⠁⠀⠀⠀⠁⠘⢎⣸⣺⣿⣿⣿⢿⣿⣽⣿⣿⣿⣿⣿⣿⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢰⣿⡿⣿⣿⢣⣿⢿⣿⢗⣽⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣀⣀⣀⣀⣀⣀⣀⣀⣠⣤⣴⣶⣿⣿⣿⡾⣿⣿⣷⢽⣷⣿⣿⣿⣿⣿⣿⣿⣾⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣾⣰⣿⣯⢞⣷⣿⣟⣅⣿⣿⣿⠿⠿⠿⠿⠿⠛⠛⠛⠿⣿⣯⡿⡫⡡⠽⣿⠽⠟⣿⣿⣿⣿⣿⣿⣺⣿⣿⣗⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣻⣆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣇⣿⣽⢳⣿⣿⠱⣼⡟⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣯⠾⠚⠉⠀⠀⠀⣟⢛⡿⠛⠉⣻⣿⣿⣿⣻⡧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣧⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢿⡿⣯⣽⣺⣿⣟⢴⢟⣥⣛⡇⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠋⠉⠀⠀⠀⠀⠀⠀⣻⠞⠁⠀⠀⣯⣿⣿⣿⣟⣕⣿⡿⣿⣿⣿⣿⣿⣿⣫⡯⣯⣿⣷⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⡺⣺⡿⠿⣾⣿⢣⡭⢿⠀⠹⠖⢶⡀⠀⠀⠀⠀⠀⡼⢭⣉⡀⠀⠀⠀⠀⠀⢀⡜⠁⠀⠀⠀⠸⣿⣿⣿⣿⢷⣗⢿⣿⡿⢋⣹⣿⣿⣿⣿⡪⡿⣾⣺⣿⣄⠀⠀⠀
+⠀⠀⠀⢀⡼⣯⣾⣯⣯⣽⣿⣿⠜⢦⡤⠀⠀⢲⡟⠀⠀⠀⠀⠀⢐⡇⠀⠀⠉⠛⠒⠶⠶⣶⠋⠀⠀⠀⠀⠀⣮⣾⣿⣿⣿⣟⣿⣝⢽⠞⣑⢑⣿⣿⣿⣿⡯⣯⣻⣯⣯⣽⣦⠀⠀
+⠀⠀⢠⣻⣮⣿⣟⣜⢿⢽⣿⣟⠀⡸⣤⡼⠷⡶⠇⠀⠀⠀⠀⠀⢸⢤⡀⠀⠀⠀⠀⢀⡴⠁⠀⠀⠀⠀⠀⢰⣻⣿⣿⢿⣿⡯⣿⣷⢿⣇⠆⠄⣿⣿⣿⣿⣿⡿⣾⣿⣷⡿⢿⣆⠀
+⠀⣰⣿⣷⣿⣿⣷⣝⣵⢽⡿⠫⠰⠸⢸⢸⠸⣿⢇⣀⣠⣄⠀⠀⢸⠀⠀⠀⠀⠉⢉⡟⠀⠀⠀⠀⠀⠀⠀⣇⣿⣿⡏⣾⣿⡿⣿⣿⣺⡻⣴⠙⢾⣿⣿⣿⣿⣷⣿⢿⣿⣿⣺⡻⡇
+⢸⡧⢽⣿⣿⣿⣿⠝⣷⠻⠟⢻⡆⠊⡮⠊⣇⠟⣿⡈⠃⠼⣆⠀⠸⠀⠀⠀⠀⢀⠎⠀⠀⠀⠀⠀⠀⠀⢰⣕⢿⣿⢿⢯⣿⣿⣿⣿⣮⡯⡷⣎⣻⣿⣿⣿⣿⣿⣿⣽⣿⣿⣫⡯⣧
+⠈⢿⢿⣿⣿⣿⣿⠇⡟⢀⠀⢈⣹⡆⣸⡯⢻⣌⠷⢶⣀⡶⠛⠀⠀⠀⠀⠀⢀⡎⠀⠀⠀⠀⠀⢀⣠⣲⡿⣗⣿⣿⣽⣗⣿⣿⣿⣿⣿⣿⡫⣿⣿⣿⣿⣿⣿⣿⣗⣿⣿⣿⡷⣪⣿
+⠀⠀⠛⣿⣿⣿⣿⢵⣽⣟⠦⠞⠁⡊⡪⣿⢨⡿⣯⣳⣭⣤⡀⠀⢠⠀⠀⠀⡘⠀⠀⠀⢀⣤⣾⡿⢋⣥⢾⢷⣷⢿⣇⢿⣏⣿⣿⣿⣿⣯⡿⣯⣺⣿⣿⣿⣵⣿⣿⣿⣿⣿⡯⡫⡎
+⠀⠀⠀⠈⢿⣿⣿⡟⣿⣧⣿⣬⠠⢀⠡⢣⣼⡧⠊⠻⣝⣿⣿⢆⢸⠀⠀⢰⠁⢀⣠⣾⡿⠟⣡⠔⠋⠔⡀⡼⣟⣾⣽⣄⣿⣝⣿⣿⣿⣷⣿⣿⢻⣎⣿⣽⣿⣿⡿⣿⣿⣿⡯⣻⠇
+*/
